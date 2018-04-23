39285c0833b2c76540d1a315ec059ef4c894b6f1
irontrack <hitchhikes4mail@gmail.com>
2018-04-23 15:32:51 -0700

    lab 2 stuff

--- trajectory.m
+++ trajectory.m
@@ -2,72 +2,85 @@
 
 launch_angle = 5;
 dt = 0.001;
-time = 0;
-
-CD_V = input('vehicle CD:');
-CD_P = input('parachute CD:');
-
 % front vehicle angle
-A_v = (pi/4)*(0.042)^2;
-A_p = (pi/4)*(.4572)^2;
+A_v = (pi/4)*(0.0396)^2;
+A_p = (pi/4)*(2.54*12/100)^2;
 
-V_0 = 0;
+CD_V = [0;0.5;0.8];
+CD_P = [0 ;1.5 ; 2]; 
 
-Y_0 = 0;
-i = 1;
-% initial mass
-mass = .071 ;
-cont = true;
-while cont
-    
-    if time == 0
-        Y(i) = Y_0;
-        V(i) = V_0;
-    end
-    % burn phase
-    if time < 0.857
-        
-        mdot = thrust(time)/(9.81*78.81);
-        mass = mass - mdot*dt;
-        D = CD_V*0.5*density(Y(i))*(V(i)^2)*A_v;
-        
-        dv = (thrust(time)/mass)*dt - (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
-        V(i + 1) = V(i) + dv; 
-        Y(i + 1) = Y(i) + V(i + 1)*dt;
-    end
-    
-    % coast phase
-    if (time > 0.857) && (time < 4.857)
+
+for j = 1:3
+    clearvars Y X
+    time = 0;
   
-        D = CD_V*0.5*density(Y(i))*(V(i)^2)*A_v;
-        if(V(i) > 0 )
-            D = -D;
+
+    V_0 = 0;
+
+    Y_0 = 0;
+    i = 1;
+    % initial mass
+    mass = 0.037 ;
+    cont = true;
+    while cont
+
+        if time == 0
+            Y(i) = Y_0;
+            V(i) = V_0;
         end
-        dv = (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
-        V(i + 1) = V(i) + dv; 
-        Y(i + 1) = Y(i) + V(i + 1)*dt;
-    end
-        
-    % desent phase
-    if (time > 4.857)
-        
-        
-        D = CD_P*0.5*density(Y(i))*(V(i)^2)*A_p;
-        if(V(i) > 0 )
-            D = -D;
+        % burn phase
+        if time < 2.024
+
+            mdot = thrust(time)/(9.81*83.25);
+            mass = mass - mdot*dt;
+            D = CD_V(j)*0.5*density(Y(i))*(V(i)^2)*A_v;
+
+            dv = (thrust(time)/mass)*dt - (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
+            V(i + 1) = V(i) + dv; 
+            Y(i + 1) = Y(i) + V(i + 1)*dt;
         end
-        dv = (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
-        V(i + 1) = V(i) + dv; 
-        Y(i + 1) = Y(i) + V(i + 1)*dt;
+
+        % coast phase
+        if (time > 2.024) && (time < 4.024)
+
+            D = CD_V(j)*0.5*density(Y(i))*(V(i)^2)*A_v;
+            if(V(i) > 0 )
+                D = -D;
+            end
+            dv = (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
+            V(i + 1) = V(i) + dv; 
+            Y(i + 1) = Y(i) + V(i + 1)*dt;
+        end
+
+        % desent phase
+        if (time > 4.024)
+
+
+            D = CD_P(j)*0.5*density(Y(i))*(V(i)^2)*A_p;
+            if(V(i) > 0 )
+                D = -D;
+            end
+            dv = (D/mass)*dt - 9.81*cosd(launch_angle)*dt;
+            V(i + 1) = V(i) + dv; 
+            Y(i + 1) = Y(i) + V(i + 1)*dt;
+        end
+
+        if(Y(i)<0)&&(time > 1)
+            cont = false;
+        end
+        i = i + 1;
+        time = time + dt;
+
+
+
     end
-    
-    if(Y(i)<0)&&(time > 1)
-        cont = false;
-    end
-    i = i + 1;
-    time = time + dt;
-    
-    
-    
+    t = 0:dt:(i - 1)*dt;
+    plot(t,Y)
+    hold on
+    fprintf('landing velocity: %6.3f\n',V(i - 10))
 end
 
+
+grid on
+legend('no drag','CD_v = 0.5','CD_v = 0.8')
+ 
--- van_karmen.m
+++ van_karmen.m
@@ -1,6 +1,6 @@
 % main driver for van karmen line sim
 
- for j = 1:6
+
 launch_angle = 0;
 dt = 0.001;
 
@@ -9,6 +9,7 @@ s = 1.5;
 presure = 300;
 sigma_y = 45000;
 thickness = (s*presure*d)/(2*sigma_y);
+fprintf('thickness: %6.3f\n',thickness)
 m_pay = 400/32.175;
 
 hoop_area = (pi)*(((d/2)^2) - (d/2 - thickness)^2);
@@ -18,81 +19,93 @@ m_unit_length = rho_a*hoop_area;
 
 fprintf('mass per unit length: %4.3f\n',m_unit_length)
 
+for k = 6:6
+    
+    clearvars V Y
+    max_apogee = ones(6,1);
+    rocket_length = [30;33;36;39;42;45;48;51];
+    for j = 30:3:51
+    CD_V = 0.75;
+    CD_P = 0.75;
+    % front vehicle angle
+    A_v = (pi/4)*(1.25)^2;
+    A_p = (pi/4)*(1.25)^2;
+
+    V_0 = 0;
+
+    Y_0 = 0;
+    vcm = 327360; %ft;
+    isp = 200:10:250;
+    thrust = 4500;
+    mdot = thrust/(32.175*isp(k));
+    A_tank = pi*((d/2 - thickness)^2);
+    % alpha = (sqrt(2*32.175*vcm)+ 32.175*A_tank*50/mdot)/(32.175*isp(j) );
+    % 
+    % length = m_pay*(exp(alpha) - 1)/(m_unit_length*(1 - exp(alpha)) + A_tank*rho_prop);
+    length = j;
+    m_prop = pi*((d/2 - thickness)^2)*length*rho_prop;
+    m_tank = m_unit_length*length;
+
+    % initial mass
 
 
-CD_V = 0.75;
-CD_P = 0.75;
-% front vehicle angle
-A_v = (pi/4)*(1.25)^2;
-A_p = (pi/4)*(1.25)^2;
 
-V_0 = 0;
+        cont = true;
+        i = 1;
+        time = 0;
+        mass = m_pay + m_prop + m_tank; 
+        Y = zeros(1e6,1);
+        V = zeros(1e6,1);
+        while cont
 
-Y_0 = 0;
-vcm = 327360; %ft;
-isp = 200:10:250;
-thrust = 4500;
-mdot = thrust/(32.175*isp(j));
-A_tank = pi*((d/2 - thickness)^2);
-alpha = (sqrt(2*32.175*vcm)+ 32.175*A_tank*50/mdot)/(32.175*isp(j) );
-
-length = m_pay*(exp(alpha) - 1)/(m_unit_length*(1 - exp(alpha)) + A_tank*rho_prop);
-m_prop = pi*((d/2 - thickness)^2)*length*rho_prop;
-m_tank = m_unit_length*length;
-
-% initial mass
-
-
-
-    cont = true;
-    i = 1;
-    time = 0;
-    mass = m_pay + m_prop + m_tank; 
-    while cont
-
-        if time == 0
-            Y(i) = Y_0;
-            V(i) = V_0;
-        end
-        % burn phase
-        if mass > (m_pay + m_tank)
-
-            
-            mass = mass - mdot*dt;
-            D = CD_V*0.5*0.00194*density(0.3048*Y(i))*(V(i)^2)*A_v;
-
-            dv = (thrust/mass)*dt - (D/mass)*dt - 32.175*cosd(launch_angle)*dt;
-            V(i + 1) = V(i) + dv; 
-            Y(i + 1) = Y(i) + V(i + 1)*dt;
-        end
-
-        % coast phase
-        if mass <= (m_pay + m_tank)
-
-            D = CD_V*0.5*0.00194*density(0.3048*Y(i))*(V(i)^2)*A_v;
-            if(V(i) > 0 )
-                D = -D;
+            if time == 0
+                Y(i) = Y_0;
+                V(i) = V_0;
             end
-            dv = (D/mass)*dt - 32.175*cosd(launch_angle)*dt;
-            V(i + 1) = V(i) + dv; 
-            Y(i + 1) = Y(i) + V(i + 1)*dt;
+            % burn phase
+            if mass > (m_pay + m_tank)
+
+
+                mass = mass - mdot*dt;
+                D = CD_V*0.5*0.00194*density(0.3048*Y(i,1))*(V(i,1)^2)*A_v;
+
+                dv = (thrust/mass)*dt - (D/mass)*dt - 32.175*cosd(launch_angle)*dt;
+                V(i + 1) = V(i,1) + dv; 
+                Y(i + 1) = Y(i,1) + V(i + 1,1)*dt;
+            end
+
+            % coast phase
+            if mass <= (m_pay + m_tank)
+
+                D = CD_V*0.5*0.00194*density(0.3048*Y(i,1))*(V(i,1)^2)*A_v;
+                if(V(i) > 0 )
+                    D = -D;
+                end
+                dv = (D/mass)*dt - 32.175*cosd(launch_angle)*dt;
+                V(i + 1,1) = V(i,1) + dv; 
+                Y(i + 1,1) = Y(i,1) + V(i + 1,1)*dt;
+            end
+
+
+            if(Y(i)<0)&&(time > 1)
+                cont = false;
+            end
+            i = i + 1;
+            time = time + dt;
+
+
+
         end
-
-       
-        if(Y(i)<0)&&(time > 1)
-            cont = false;
-        end
-        i = i + 1;
-        time = time + dt;
-
-
-
-    end
-    
-    t = 0:dt:(i-1)*dt;
-    plot(t,Y)
+        % creat data for max apogee versus length
+        max_apogee((j/3) - 9) = max(Y);
+        
+    end 
+    plot(rocket_length,max_apogee)
     hold on
-    fprintf('length:%5.3f\n',length)
-    
- end
 
+end
+grid on
+% legend('ISP_2_0_0','ISP_2_1_0','ISP_2_2_0','ISP_2_3_0','ISP_2_4_0','ISP_2_5_0')
+xlabel('length (feet)')
+ylabel('altitude (feet)')
+title('Max Apogee vs Rocket Length varying ISP')
